0c1e63b3c45b8d714b82608d24e01e816934bc0c
Gonzalo Mejía <gonzmejiasan@hotmail.com>
2022-08-10 12:45:20 -0500

    Agregando selects

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function header() {
   return (
     <>
-          <header class="py-5 bg-light border-bottom mb-4">
+          <header class="py-2 bg-light border-bottom mb-4">
         <div class="container">
           <div class="text-center my-5">
             <h1 class="fw-bolder">Welcome to Blog Home!</h1>
--- src/components/search.js
+++ src/components/search.js
@@ -9,15 +9,20 @@ function Search() {
   const [stores, setStores] = useState([]);
   const [districtsByProvince, setDistrictsByProvince] = useState([]);
   const [storesByDistrict, setStoresByDistrict] = useState([]);
+  const [provinceId, setProvinceId] = useState('');
+  const [districtId, setDistrictId] = useState('');
   const provincesRef = collection(db, "provincias");
   const districtsRef = collection(db, "distritos");
   const storesRef = collection(db, "tiendas");
 
   const handleProvinceChange = (event) => {
-    let filteredProvinces = districts.filter(
+    let filteredDistricts = districts.filter(
       (district) => district.id_ref == event.target.value
     );
-    setDistrictsByProvince(filteredProvinces);
+    setDistrictsByProvince(filteredDistricts);
+    setProvinceId(event.target.value);
+    setStoresByDistrict([]);
+    setDistrictId(0);
   };
 
   const handleDistrictChange = (event) => {
@@ -25,6 +30,7 @@ function Search() {
       (store) => store.id_ref == event.target.value
     );
     setStoresByDistrict(filteredStores);
+    setDistrictId(event.target.value);
   };
 
   useEffect(() => {
@@ -35,24 +41,54 @@ function Search() {
 
     const getDistricts = async () => {
       const data = await getDocs(districtsRef);
-      setDistricts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      const dataList = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      setDistricts(dataList);
+      return dataList;
     };
 
     const getStores = async () => {
       const data = await getDocs(storesRef);
-      setStores(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      const dataList = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      setStores(dataList);
+      return dataList;
     };
 
     getProvinces();
-    getDistricts();
-    getStores();
+    const districtList = getDistricts();
+    const storeList = getStores();
+
+    //Inicializo los selects
+    districtList.then((result) => {
+      let filteredDistricts = result.filter(
+        (district) => district.id_ref == 15
+      );
+      setDistrictsByProvince(filteredDistricts);
+      setProvinceId('15');
+      setDistrictId('1');
+
+      //Cargo tiendas
+      storeList.then((result) => {
+        let filteredStores = result.filter(
+          (store) => store.id_ref == 1
+        );
+        setStoresByDistrict(filteredStores);
+      })
+    })
+
   }, []);
 
   return (
     <>
       <div className="container">
         <div className="row">
-          <div className="col-lg-3">
+          <div className="col-lg-12 py-2">
+            <button className="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#filtros" aria-expanded="false" aria-controls="filtros">
+              Ocultar Filtros
+            </button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-3 collapse show collapse-horizontal" id="filtros">
             <div className="card mb-4">
               <div className="card-header">Búsqueda</div>
               <div className="card-body">
@@ -60,8 +96,8 @@ function Search() {
                   <input
                     className="form-control"
                     type="text"
-                    placeholder="Escribe una palabra..."
-                    aria-label="Escribe una palabra..."
+                    placeholder="Ingresa una palabra..."
+                    aria-label="Ingresa una palabra..."
                     aria-describedby="button-search"
                   />
                 </div>
@@ -70,13 +106,15 @@ function Search() {
             <div className="card mb-4">
               <div className="card-header">Filtros</div>
               <div className="card-body">
-                <div className="form-group">
+                <div className="form-group p-2">
                   <label htmlFor="provinceSelect">Provincias</label>
                   <select
                     className="form-control"
                     id="provinceSelect"
+                    value={provinceId}
                     onChange={handleProvinceChange}
                   >
+                    <option value="0">--Seleccione Provincia--</option>
                     {provinces.map((province) => {
                       return (
                         <option key={province.id} value={province.id_ref}>
@@ -86,13 +124,15 @@ function Search() {
                     })}
                   </select>
                 </div>
-                <div className="form-group">
+                <div className="form-group p-2">
                   <label htmlFor="districtsSelect">Distritos</label>
                   <select
                     className="form-control"
                     id="districtsSelect"
+                    value={districtId}
                     onChange={handleDistrictChange}
                   >
+                    <option value="0">--Seleccione Distrito--</option>
                     {districtsByProvince.map((district) => {
                       return (
                         <option key={district.id} value={district.id_dist}>
@@ -112,6 +152,7 @@ function Search() {
               </div>
             </div>
           </div>
+
           <div className="col-lg-9">
             <div className="row">
               <Results stores={storesByDistrict} />
